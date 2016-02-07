0c95529f78dd0a38a4d91475b3f43625205f9be7
dolewski <dolewski@student.42.fr>
2016-02-07 08:42:04 +0100

    rm script for home

--- scripts/ToggleTouchPad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-stat=`synclient | grep "TouchpadOff" | cut -d\= -f2`
-if [ $stat -eq 0 ]; then
-    synclient TouchpadOff=1 ;
-    notify-send "Touchpad disabled"
-else
-    synclient TouchpadOff=0 ;
-    notify-send "Touchpad enabled"
-fi
